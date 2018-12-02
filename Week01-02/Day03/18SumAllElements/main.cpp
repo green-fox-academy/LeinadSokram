eb275631fafbc0c2febe3c1df8d26c83cd7f5f3a
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 17:43:30 +0100

    style (reformatting code)

--- Week01-02/Day03/18SumAllElements/main.cpp
+++ Week01-02/Day03/18SumAllElements/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
     // - Create an array variable named `ai`
     //   with the following content: `[3, 4, 5, 6, 7]`
     // - Print the sum of the elements in `ai`
 
-    int ai[] = {3,4,5,6,7};
+    int ai[] = {3, 4, 5, 6, 7};
     int initialNumber = 0;
 
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < sizeof(ai) / sizeof(ai[0]); i++) {
         initialNumber += ai[i];
     }
 
