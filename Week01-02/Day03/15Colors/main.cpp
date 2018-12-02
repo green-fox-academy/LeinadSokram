517f2fa03ec71fc901169c9d34812a9e849c3ecd
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 17:25:18 +0100

    style (reformatting code)

--- Week01-02/Day03/15Colors/main.cpp
+++ Week01-02/Day03/15Colors/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char* args[])
+{
     // - Create a two dimensional array
     //   which can contain the different shades of specified colors
     // - In `colors[0]` store the shades of green:
@@ -12,17 +12,8 @@ int main(int argc, char* args[]) {
     // - In `colors[2]` store the shades of pink:
     //   `"orchid", "violet", "pink", "hot pink"`
 
-    std::string greenArray[5] = {"lime", "forest green", "olive", "pale green", "spring green"};
-    std::string redArray[3] = {"orange red", "red", "tomato"};
-    std::string purpleArray[4] = {"orchid", "violet", "pink", "hot pink"};
-
-    std::string allColorsArray[3][5] = {{greenArray},{redArray},{purpleArray}};
-
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 5, j++){
-            std::cout << i << "," << j << std::endl;
-        }
-
+    std::string allColorsArray[3][5] = {{"lime", "forest green", "olive", "pale green", "spring green"},
+                                        {"orange red", "red", "tomato"},{"orchid", "violet", "pink", "hot pink"}};
 
     return 0;
 }
