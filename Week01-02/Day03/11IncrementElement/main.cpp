91b4eec87ac7e675b296c6085d797808125a8cd2
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 14:22:31 +0100

    style (reformatting code)

--- Week01-02/Day03/11IncrementElement/main.cpp
+++ Week01-02/Day03/11IncrementElement/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
     // - Create an array variable named `t`
     //    with the following content: `[1, 2, 3, 4, 5]`
     // - Increment the third element
     // - Print the third element
 
-    int t[] = {1,2,3,4,5};
-
+    int t[] = {1, 2, 3, 4, 5};
     t[2] = t[2] + 3;
-
     std::cout << t[2] << std::endl;
 
     return 0;
