790a698b031b843382c187e915e1dd3d34735d39
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 17:40:43 +0100

    style (reformatting code)

--- Week01-02/Day03/17SwapElements/main.cpp
+++ Week01-02/Day03/17SwapElements/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char* args[])
+{
     // - Create an array variable named `abc`
     //   with the following content: `["first", "second", "third"]`
     // - Swap the first and the third element of `abc`
 
     std::string abc[3] = {"first", "second", "third"};
-
     abc[0].swap(abc[2]);
     std::cout << "The first element is: " << abc[0] << std::endl;
     std::cout << "The third element is: " << abc[2] << std::endl;
