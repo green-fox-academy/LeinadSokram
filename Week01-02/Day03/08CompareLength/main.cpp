d5f0f855c70e7f49bee408c0d68ca79aa7ff70ef
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 14:19:50 +0100

    style (reformatting code)

--- Week01-02/Day03/08CompareLength/main.cpp
+++ Week01-02/Day03/08CompareLength/main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
     // - Create an array variable named `p1`
     //   with the following content: `[1, 2, 3]`
     // - Create an array variable named `p2`
     //   with the following content: `[4, 5]`
     // - Print if `p2` has more elements than `p1`
 
-    int p1[] = {1,2,3};
-    int p2[] = {4,5};
+    int p1[] = {1, 2, 3};
+    int p2[] = {4, 5};
 
-    if (sizeof(p1) / sizeof(p1[0]) < sizeof(p2) / sizeof(p2[0])){
+    if (sizeof(p1) / sizeof(p1[0]) < sizeof(p2) / sizeof(p2[0])) {
         std::cout << "P2 has fewer elements than P1." << std::endl;
-    }else{
+    } else {
         std::cout << "P2 has more elements than P1." << std::endl;
     }
 
