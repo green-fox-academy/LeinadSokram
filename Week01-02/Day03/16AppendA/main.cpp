8cc4b8c1be6890869baa8363f55aca7358260231
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 17:39:47 +0100

    style (reformatting code)

--- Week01-02/Day03/16AppendA/main.cpp
+++ Week01-02/Day03/16AppendA/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
     // - Create an array variable named `animals`
     //   with the following content: `["koal", "pand", "zebr"]`
     // - Add all elements an `"a"` at the end
@@ -10,11 +10,10 @@ int main(int argc, char* args[]) {
     std::string animals[3] = {"koal", "pand", "zebr"};
     std::string appendA = "a";
 
-    for (int i = 0; i < 3; i++){
-        animals[i].push_back(appendA);
+    for (int i = 0; i < 3; i++) {
+        animals[i] += "a";
         std::cout << animals[i] << std::endl;
     }
-
 
     return 0;
 }
