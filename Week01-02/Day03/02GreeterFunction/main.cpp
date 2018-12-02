370634633ecddff090e9bc2a6fa014c128c5f7bb
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 13:44:46 +0100

    style (reformatting code)

--- Week01-02/Day03/02GreeterFunction/main.cpp
+++ Week01-02/Day03/02GreeterFunction/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <string>
 
-std::string greet(std::string y);
+std::string greet(std::string name);
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char *args[])
+{
     // - Create a string variable named `al` and assign the value `Greenfox` to it
     // - Create a function called `greet` that greets it's input parameter
     // - Greeting is printing e.g. `Greetings dear, Greenfox`
     // - Greet `al`
 
     std::string al = "Greenfox";
-
-    std::cout << "Greetings dear, " << greet(al) << std::endl;
+    greet(al);
 
     return 0;
 }
 
-std::string greet(std::string y){
-
-    return y;
-
+std::string greet(std::string name)
+{
+    std::cout << "Greetings dear, " << name << std::endl;
 };
