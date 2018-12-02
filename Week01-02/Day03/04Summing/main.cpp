afec349e4e8277b1d4bf3ae1a7cca5195469f1ea
LeinadSokram <markos.daniel@mensa.hu>
2018-12-02 13:52:17 +0100

    style (reformatting code)

--- Week01-02/Day03/04Summing/main.cpp
+++ Week01-02/Day03/04Summing/main.cpp
@@ -3,28 +3,26 @@
 
 int sum(int numberFromMain);
 
-int main(int argc, char* args[]) {
-
+int main(int argc, char* args[])
+{
     // - Write a function called `sum` that sum all the numbers
     //   until the given parameter and returns with an integer
 
     int givenParameter;
     std::cout << "Please enter a parameter: " << std::endl;
     std::cin >> givenParameter;
-
     std::cout << "The sum of all numbers until the given parameter is: " << sum(givenParameter) << std::endl;
 
     return 0;
 }
 
-int sum(int numberFromMain){
-
+int sum(int numberFromMain)
+{
     int totalSum = 0;
 
-    for(int i = 0; i <= numberFromMain; i++){
-        totalSum += + i;
+    for(int i = 0; i <= numberFromMain; i++) {
+        totalSum += i;
     }
 
     return totalSum;
-
 }
