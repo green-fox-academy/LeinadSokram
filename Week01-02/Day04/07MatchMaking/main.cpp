66431363c77bf60f874adeaf88bcfc77ed18ad29
LeinadSokram <markos.daniel@mensa.hu>
2018-12-03 16:27:33 +0100

    style (reformatting code)

--- Week01-02/Day04/07MatchMaking/main.cpp
+++ Week01-02/Day04/07MatchMaking/main.cpp
@@ -2,31 +2,32 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> makingMatches(const std::vector<std::string>& girls, const std::vector<std::string>& boys)
+std::vector<std::string> makingMatches(const std::vector<std::string> &girls, const std::vector<std::string> &boys)
 {
     std::vector<std::string> newGirls = girls;
     std::vector<std::string> newBoys = boys;
     std::vector<std::string> pairs;
 
-    for(int g = 0; g < newGirls.size(); ++g){
-        pairs.insert(pairs.begin(0), newGirls[g]);
-        pairs.insert(pairs.begin(1), newBoys[b])
-        }
+    for (int i = 0; i < newBoys.size(); i++)
+    {
+        pairs.push_back(newGirls[i]);
+        pairs.push_back(newBoys[i]);
+    }
 
-
+    return pairs;
 }
 
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
-    std::vector<std::string> girls = {"Eve","Ashley","Claire","Kat","Jane"};
-    std::vector<std::string> boys = {"Joe","Fred","Tom","Todd","Neef","Jeff"};
+    std::vector<std::string> girls = {"Eve", "Ashley", "Claire", "Kat", "Jane"};
+    std::vector<std::string> boys = {"Joe", "Fred", "Tom", "Todd", "Neef", "Jeff"};
 
     // Write a method that joins the two lists by matching one girl with one boy into a new list
     // Expected output: "Eve", "Joe", "Ashley", "Fred"...
 
-    for(const auto& pairs : makingMatches(girls, boys))
+    for (const auto &pairs : makingMatches(girls, boys))
     {
-        std::cout << pairs << " ";
+        std::cout << pairs << ", ";
     }
 
     return 0;
--- Week01-02/Day04/08AppendLetter/main.cpp
+++ Week01-02/Day04/08AppendLetter/main.cpp
@@ -2,25 +2,28 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char* args[])
+std::vector<std::string> appendA(std::vector<std::string> animalsNoA);
 
-std::vector<std::string> appendA(std::vector<std::string> animals)
-{
-
-
-
-}
-
+int main(int argc, char *args[])
 {
     std::vector<std::string> animals = {"bo", "anacond", "koal", "pand", "zebr"};
 
     // Create a function called "appendA()" that adds "a" to every string in the "animals" list.
     // The parameter should be a vector.
 
-    for(const auto& animal : appendA(animals))
-    {
+    for (const auto &animal : appendA(animals)) {
         std::cout << animal << " ";
     }
 
     return 0;
+}
+
+std::vector<std::string> appendA(std::vector<std::string> animalsNoA)
+{
+    std::vector<std::string> animalsA = animalsNoA;
+    for (int i = 0; i < animalsA.size(); ++i) {
+        animalsA[i] += "a";
+    }
+
+    return animalsA;
 }
