22c712734372b756005a116b8dda336a277a0114
LeinadSokram <markos.daniel@mensa.hu>
2018-12-03 15:08:28 +0100

    style (reformatting code)

--- Week01-02/Day04/06SolarSystem/main.cpp
+++ Week01-02/Day04/06SolarSystem/main.cpp
@@ -4,11 +4,8 @@
 
 std::vector<std::string> putSaturn(const std::vector<std::string> &planets)
 {
-    //Write a function that returns all planets to the main program
-    //Keep in mind that these are in a constant vector ie. you can't modify it!
-
     std::vector<std::string> planet = planets;
-    planet.insert(planet.begin(5), "Saturn");
+    planet.emplace_back("Saturn");
 
     return planet;
 }
