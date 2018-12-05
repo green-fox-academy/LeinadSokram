bf81359d17bf525d959673b76760ed6dcca3448c
LeinadSokram <markos.daniel@mensa.hu>
2018-12-05 15:21:30 +0100

    chore (Old enough)

--- Week07/07OldEnough/main.c
+++ Week07/07OldEnough/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* isOldEnough(int ageToCheck);
+
 int main()
 {
     // Create a program which asks for the age of the user and stores it
@@ -10,8 +12,23 @@ int main()
     int age;
     printf("How old are you?\n");
     scanf("%d", &age);
-
-
+    printf(isOldEnough(age));
 
     return 0;
+}
+
+char* isOldEnough(int ageToCheck)
+{
+    int ageToBuy = 18;
+    int canBuy = 0;
+
+    if (ageToCheck >= ageToBuy) {
+        canBuy = 1;
+    }
+
+    if (canBuy) {
+        return "Old enough to buy alcohol!";
+    } else {
+        return "Too young to buy alcohol!";
+    }
 }
