53a790cd6ea84b1a78bd2204630a372e9eb4d317
LeinadSokram <markos.daniel@mensa.hu>
2018-12-10 11:48:30 +0100

    chore (c length)

--- Week07/Day03/12Length/main.c
+++ Week07/Day03/12Length/main.c
@@ -6,22 +6,33 @@ int lengthReturner(char *word);
 
 int lengthReturnerWithoutStringH(char *wordWithoutStringH);
 
-main()
+int main()
 {
     // Create a program which asks for the name of the user and stores it
     // Create a function which takes a string as a parameter and returns the length of it
     // Solve this exercise with and without using string.h functions
 
-    char *userName;
+    char userName[20];
     printf("Enter a username:\n");
-    scanf("%s", &userName);
+    scanf("%s", userName);
 
-    printf(lengthReturner(userName));
+    printf("The name is %d characters long (with string.h).\n", lengthReturner(userName));
+    printf("The name is %d characters long (without string.h).", lengthReturnerWithoutStringH(userName));
 
     return 0;
 }
 
 int lengthReturner(char *word)
 {
+    return strlen(word);
+}
 
+int lengthReturnerWithoutStringH(char *wordWithoutStringH)
+{
+    int wordLength = 0;
+    for (int i = 0; i < strlen(wordWithoutStringH); ++i) {
+        wordLength++;
+    }
+
+    return wordLength;
 }
--- Week07/Day03/13AreTheSame/main.c
+++ Week07/Day03/13AreTheSame/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int areTheSame(char *word1, char *word2);
+
 main()
 {
     // Create a program which asks for two strings and stores them
@@ -9,5 +11,20 @@ main()
     // returns 1 if the two strings are the same and 0 otherwise
     // Try to erase small and uppercase sensitivity.
 
+    char *firstString;
+    printf("Enter a word:\n");
+    scanf("%s", &firstString);
+
+    char *secondString;
+    printf("Enter another word:\n");
+    scanf("%s", &secondString);
+
+    areTheSame(firstString, secondString);
+
     return 0;
+}
+
+int areTheSame(char *word1, char *word2)
+{
+
 }
