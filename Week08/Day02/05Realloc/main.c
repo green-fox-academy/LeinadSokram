b0e69a13e1e0a4f1c3b615349f2497e66f67a2ee
LeinadSokram <markos.daniel@mensa.hu>
2018-12-11 15:28:36 +0100

    chore (realloc)

---
+++ Week08/Day02/05Realloc/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prompt the user to enter a number. This number will be X.
+// Allocate memory for X numbers and prompt the user to enter X numbers.
+// Allocate memory for the sum of the X numbers(Y) in the same array
+// Fill the array with numbers from 1 to Y, then print them
+
+int main()
+{
+    int* pointer = NULL;
+
+    return 0;
+}
