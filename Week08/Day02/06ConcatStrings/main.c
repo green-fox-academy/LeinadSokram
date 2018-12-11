2effcbd5e6161963464d52da3718055d98056107
LeinadSokram <markos.daniel@mensa.hu>
2018-12-11 17:42:47 +0100

    chore (realloc)

---
+++ Week08/Day02/06ConcatStrings/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mem.h>
+
+// write a function which takes 2 strings as parameter, concatenates them together and returns it back.
+// Test it for long and empty strings as well.
+// Try to use the least amount of memory that's possible.
+
+char *concatenater(char *first, char *second);
+
+int main()
+{
+    char *first_word;
+    char *second_word;
+
+    getchar()
+
+    printf("Enter a word:\n");
+    scanf("%s", &first_word),
+
+    printf("Enter another word:\n");
+    scanf("%s", &second_word),
+
+    printf(concatenater(first_word, second_word));
+
+    return 0;
+}
+
+char *concatenater(char *first, char *second)
+{
+    char *new_word;
+
+    new_word = first;
+    strcat(new_word, second);
+
+    return new_word;
+}
