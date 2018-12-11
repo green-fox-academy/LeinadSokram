99eacf6d7a35b8f1ebd707b246f89f8392b16253
LeinadSokram <markos.daniel@mensa.hu>
2018-12-11 15:06:56 +0100

    chore (merge)

--- Week08/Day02/04Merge/main.c
+++ Week08/Day02/04Merge/main.c
@@ -15,27 +15,49 @@ int main()
     int *odd;
     int even_count = 2;
     int odd_count = 1;
+    int size = 10;
 
-    even = (int *) malloc(10 * sizeof(int));
+    even = (int *) malloc(size * sizeof(int));
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < size; ++i) {
         even[i] = even_count;
         even_count += 2;
     }
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < size; ++i) {
         printf("%d\n", even[i]);
     }
 
-    odd = (int *) malloc(10 * sizeof(int));
+    odd = (int *) malloc(size * sizeof(int));
 
-    for (int i = 0; i < 10; ++i) {
-        odd[i] = odd_count;
+    for (int j = 0; j < size; ++j) {
+        odd[j] = odd_count;
         odd_count += 2;
     }
 
-    for (int i = 0; i < 10; ++i) {
-        printf("%d\n", odd[i]);
+    for (int j = 0; j < size; ++j) {
+        printf("%d\n", odd[j]);
+    }
+
+    printf("-------------------\n");
+
+    int new_size = 20;
+
+    realloc(even, new_size * sizeof(int));
+
+    for (int i = 0; i < size; ++i) {
+        printf("%d\n", even[i]);
+    }
+
+    /*for (int k = 11; k < new_size; ++k)
+        for (int l = 0; l < size; ++l) {
+        even[k] = odd[l];
+            printf("%d\n", even[k]);
+    }
+    */
+
+    for (int k = 0; k < new_size; ++k) {
+        
     }
 
     return 0;
