bf1119b620e12d87d59c4930cb4258329a6ed020
LeinadSokram <markos.daniel@mensa.hu>
2018-12-11 13:10:36 +0100

    chore (malloc)

---
+++ Week08/Day02/01Malloc/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with malloc
+// please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
+// please delete the array before the program exits
+
+
+int main()
+{
+    int* pointer = NULL;
+
+    return 0;
+}
