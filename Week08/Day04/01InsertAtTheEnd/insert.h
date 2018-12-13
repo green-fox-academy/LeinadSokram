4dacd1410ddfcb78124b188370eb221bb4b4f0e1
LeinadSokram <markos.daniel@mensa.hu>
2018-12-13 17:43:46 +0100

    chore (Insert at the End w/ comments)

---
+++ Week08/Day04/01InsertAtTheEnd/insert.h
@@ -0,0 +1,10 @@
+#ifndef INC_01INSERTATTHEEND_INSERT_H
+#define INC_01INSERTATTHEEND_INSERT_H
+
+typedef struct node //This is a struct
+{
+    int value; //value of the node
+    struct node *next; //pointer to the next node's memory address
+} node_t;
+
+#endif //INC_01INSERTATTHEEND_INSERT_H
