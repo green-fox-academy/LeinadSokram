bb8780f379614aac52cd73025d4d908aa2bd0d9b
LeinadSokram <markos.daniel@mensa.hu>
2018-12-21 15:48:40 +0100

    chore (Reaction Tester)

--- Week09/Day04/ReflexGame/src/main.c
+++ Week09/Day04/ReflexGame/src/main.c
@@ -14,6 +14,10 @@ static void CPU_CACHE_Enable(void);
 UART_HandleTypeDef uart_handle;
 RNG_HandleTypeDef rng_handle;
 
+typedef enum phase {
+	START, GAME_INIT
+} phase_t;
+
 int main(void) {
 
 	/* Configure the MPU attributes as Write Through */
@@ -91,39 +95,59 @@ int main(void) {
 	led6.Speed = GPIO_SPEED_HIGH;
 	HAL_GPIO_Init(GPIOF, &led6);
 
+	phase_t phase = START;
+
+	int round_number = 0;
+
 	printf("Let's play a game! Are you ready?\r\n");
 
 	while (1) {
+		switch (phase) {
 
-		if (HAL_GetTick() % 1000	== 0) {
+		case START:
 
-		HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_8);
-		HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_7);
-		HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_6);
-		HAL_Delay(1);
+			if (HAL_GetTick() % 100 == 0) {
+				HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_8);
+				HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_7);
+				HAL_GPIO_TogglePin(GPIOF, GPIO_PIN_6);
+				HAL_Delay(1);
+			}
 
-		}
+			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)
+					|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_10)
+					|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9)) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6, GPIO_PIN_RESET);
+				phase = GAME_INIT;
+			}
+			break;
 
-		if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)
-				|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_10)
-				|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9)) {
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);
-			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6, GPIO_PIN_RESET);
-		}
+		case GAME_INIT:
 
-		/*
+			printf("Round %d \r\nPush any button if you are ready\r\n", round_number);
 
-		for (int i = 0; i < 10; i++) {
+			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)
+								|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_10)
+								|| HAL_GPIO_ReadPin(GPIOF, GPIO_PIN_9)) {
+
 			uint32_t random_number;
 			HAL_RNG_GenerateRandomNumber(&rng_handle, &random_number);
 			float random_time = (random_number % 10 + 1);
 			HAL_Delay(random_time);
 			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);
+			uint32_t start_count = HAL_GetTick();
+			}
+
 			if (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0)) {
 				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+				uint32_t end_count = HAL_GetTick();
+				//float reaction = (end_count - start_count);
+				printf("Your reaction time is ... Get ready for the next round!\r\n");
+				round_number++;
 			}
-		} */
+		}
+
 	}
 
 	/*
@@ -148,48 +172,47 @@ int main(void) {
 	 }
 
 	 */
-
 }
 
 UART_PUTCHAR {
-	HAL_UART_Transmit(&uart_handle, (uint8_t*) &ch, 1, 0xFFFF);
-	return ch;
+HAL_UART_Transmit(&uart_handle, (uint8_t*) &ch, 1, 0xFFFF);
+return ch;
 }
 
 static void SystemClock_Config(void) {
-	RCC_ClkInitTypeDef RCC_ClkInitStruct;
-	RCC_OscInitTypeDef RCC_OscInitStruct;
+RCC_ClkInitTypeDef RCC_ClkInitStruct;
+RCC_OscInitTypeDef RCC_OscInitStruct;
 
-	/* Enable HSE Oscillator and activate PLL with HSE as source */
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 25;
-	RCC_OscInitStruct.PLL.PLLN = 432;
-	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-	RCC_OscInitStruct.PLL.PLLQ = 9;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-		Error_Handler();
-	}
+/* Enable HSE Oscillator and activate PLL with HSE as source */
+RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+RCC_OscInitStruct.PLL.PLLM = 25;
+RCC_OscInitStruct.PLL.PLLN = 432;
+RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+RCC_OscInitStruct.PLL.PLLQ = 9;
+if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	Error_Handler();
+}
 
-	/* activate the OverDrive to reach the 216 Mhz Frequency */
-	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
-		Error_Handler();
-	}
+/* activate the OverDrive to reach the 216 Mhz Frequency */
+if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+	Error_Handler();
+}
 
-	/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-	 clocks dividers */
-	RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
-			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
-		Error_Handler();
-	}
+/* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+ clocks dividers */
+RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK
+		| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+	Error_Handler();
+}
 }
 
 /**
@@ -198,9 +221,9 @@ static void SystemClock_Config(void) {
  * @retval None
  */
 static void Error_Handler(void) {
-	/* User may add here some code to deal with this error */
-	while (1) {
-	}
+/* User may add here some code to deal with this error */
+while (1) {
+}
 }
 
 /**
@@ -211,28 +234,28 @@ static void Error_Handler(void) {
  * @retval None
  */
 static void MPU_Config(void) {
-	MPU_Region_InitTypeDef MPU_InitStruct;
+MPU_Region_InitTypeDef MPU_InitStruct;
 
-	/* Disable the MPU */
-	HAL_MPU_Disable();
+/* Disable the MPU */
+HAL_MPU_Disable();
 
-	/* Configure the MPU attributes as WT for SRAM */
-	MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-	MPU_InitStruct.BaseAddress = 0x20010000;
-	MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
-	MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
-	MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-	MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
-	MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-	MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-	MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-	MPU_InitStruct.SubRegionDisable = 0x00;
-	MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
+/* Configure the MPU attributes as WT for SRAM */
+MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+MPU_InitStruct.BaseAddress = 0x20010000;
+MPU_InitStruct.Size = MPU_REGION_SIZE_256KB;
+MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+MPU_InitStruct.SubRegionDisable = 0x00;
+MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
 
-	HAL_MPU_ConfigRegion(&MPU_InitStruct);
+HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
-	/* Enable the MPU */
-	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+/* Enable the MPU */
+HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
 }
 
 /**
@@ -241,11 +264,11 @@ static void MPU_Config(void) {
  * @retval None
  */
 static void CPU_CACHE_Enable(void) {
-	/* Enable I-Cache */
-	SCB_EnableICache();
+/* Enable I-Cache */
+SCB_EnableICache();
 
-	/* Enable D-Cache */
-	SCB_EnableDCache();
+/* Enable D-Cache */
+SCB_EnableDCache();
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -259,13 +282,13 @@ static void CPU_CACHE_Enable(void) {
  */
 void assert_failed(uint8_t* file, uint32_t line)
 {
-	/* User can add his own implementation to report the file name and line number,
-	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+/* User can add his own implementation to report the file name and line number,
+ ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-	/* Infinite loop */
-	while (1)
-	{
-	}
+/* Infinite loop */
+while (1)
+{
+}
 }
 #endif
 
