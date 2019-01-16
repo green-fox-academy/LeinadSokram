2e0306013552fae54161ee259086f6327470d7b1
LeinadSokram <markos.daniel@mensa.hu>
2019-01-16 17:28:06 +0100

    chore (blinkergame final)

--- Week11/Blink_counter_game/src/main.c
+++ Week11/Blink_counter_game/src/main.c
@@ -8,18 +8,30 @@ static void CPU_CACHE_Enable(void);
 
 #define UART_PUTCHAR int __io_putchar(int ch) //this defines UART, so we can print out text on screen
 
+typedef enum game_state {
+	BLINKING, INPUT, RESULT
+} game_state_t;
+
+game_state_t game_state = BLINKING;
+
 UART_HandleTypeDef uart_handle; //This defines what 'uart' means
 GPIO_InitTypeDef button; //This defines what 'button' means
 TIM_HandleTypeDef tim_handle; //This defines what 'timer' means
+TIM_HandleTypeDef tim_handle2;
 RNG_HandleTypeDef rng_handle; //This generates a random number
 GPIO_InitTypeDef red_led;
+TIM_HandleTypeDef tim_handle3;
+TIM_OC_InitTypeDef tim_handle_pwm; //This defines PWM handler
 
 int random_counter = 0;
 int random_iteration;
+int push_counter = 0;
+int ignored_first_interrupt = 0;
 
 void uart_init() //This initializes UART
 {
-	__HAL_RCC_USART1_CLK_ENABLE();
+	__HAL_RCC_USART1_CLK_ENABLE()
+	;
 	uart_handle.Instance = USART1;
 	uart_handle.Init.BaudRate = 115200;
 	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
@@ -33,7 +45,8 @@ void uart_init() //This initializes UART
 
 void button_init() //This initializes the button (on port 11) so it perceives pushing
 {
-	__HAL_RCC_GPIOI_CLK_ENABLE();
+	__HAL_RCC_GPIOI_CLK_ENABLE()
+	;
 	button.Pin = GPIO_PIN_11;
 	button.Pull = GPIO_NOPULL;
 	button.Speed = GPIO_SPEED_FAST;
@@ -50,7 +63,7 @@ void timer_init() //This initializes the TIM2 timer
 	__HAL_RCC_TIM2_CLK_ENABLE()
 	;
 	tim_handle.Instance = TIM2;
-	tim_handle.Init.Period = 10000; //This defines the number of iterations after the counter resets (1 sec)
+	tim_handle.Init.Period = 1000; //This defines the number of iterations after the counter resets (1 sec)
 	tim_handle.Init.Prescaler = 10800; //This defines the number of clock ticks needed for the counter to increase (0,0001 sec)
 	tim_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	tim_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -58,17 +71,56 @@ void timer_init() //This initializes the TIM2 timer
 
 	HAL_NVIC_SetPriority(TIM2_IRQn, 0x01, 0x00); //This sets the priority for the interrupt
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);
-
 }
 
-void red_led_init()
+void timer_init_for_push() //This initializes the TIM2 timer
 {
-	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_TIM3_CLK_ENABLE()
+	;
+	tim_handle2.Instance = TIM3;
+	tim_handle2.Init.Period = 20000; //This defines the number of iterations after the counter resets (1 sec)
+	tim_handle2.Init.Prescaler = 10800; //This defines the number of clock ticks needed for the counter to increase (0,0001 sec)
+	tim_handle2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	tim_handle2.Init.CounterMode = TIM_COUNTERMODE_UP;
+	HAL_TIM_Base_Init(&tim_handle2);
+
+	HAL_NVIC_SetPriority(TIM3_IRQn, 0x01, 0x00); //This sets the priority for the interrupt
+	HAL_NVIC_EnableIRQ(TIM3_IRQn);
+}
+
+void rand_init() //This initializes the randomizer function
+{
+	__HAL_RCC_RNG_CLK_ENABLE()
+	;
+	rng_handle.Instance = RNG;
+	HAL_RNG_Init(&rng_handle);
+}
+
+void red_led_init() {
+	__HAL_RCC_GPIOH_CLK_ENABLE()
+	; //Don't forget to use the correct pin!
 	red_led.Pin = GPIO_PIN_6;
-	red_led.Mode = GPIO_MODE_INPUT;
+	red_led.Mode = GPIO_MODE_AF_PP; //output for leds, input for buttons
 	red_led.Pull = GPIO_PULLDOWN;
 	red_led.Speed = GPIO_SPEED_HIGH;
+	red_led.Alternate = GPIO_AF9_TIM12;
 	HAL_GPIO_Init(GPIOH, &red_led);
+
+	__HAL_RCC_TIM12_CLK_ENABLE()
+	; //This initializes the PWM mode (for this pin only!)
+	tim_handle3.Instance = TIM12;
+	tim_handle3.Init.Period = 100;
+	tim_handle3.Init.Prescaler = 1;
+	tim_handle3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	tim_handle3.Init.CounterMode = TIM_COUNTERMODE_UP;
+	HAL_TIM_PWM_Init(&tim_handle3);
+
+	tim_handle_pwm.OCMode = TIM_OCMODE_PWM1;
+	tim_handle_pwm.Pulse = 0; //Currently it's off
+	HAL_TIM_PWM_ConfigChannel(&tim_handle3, &tim_handle_pwm, TIM_CHANNEL_1);
+
+	HAL_TIM_Base_Start(&tim_handle3);
+	HAL_TIM_PWM_Start(&tim_handle3, TIM_CHANNEL_1);
 }
 
 int main(void) {
@@ -86,20 +138,21 @@ int main(void) {
 
 	BSP_LED_Init(LED_GREEN); //This calls the green led function.
 	timer_init(); //This calls the timer function
+	timer_init_for_push();
 	button_init(); //This calls the button function
 	uart_init(); //This calls the UART function
-	red_led_init();
+	red_led_init(); //This calls the red led function
+	rand_init(); //This calls the randomizer function
 
-	printf("Welcome to my game! Count the number of blinks, then push the blue button that many times!\r\n");
+	printf(
+			"Welcome to my game! Count the number of blinks, then push the blue button that many times!\r\n");
 
 	while (1) {
-
-		HAL_GPIO_WritePin(GPIOH, GPIO_PIN_6, GPIO_PIN_SET);
 
 	}
 }
 
-UART_PUTCHAR //This is for redefining UART. Don't forget to decomment the lcd_log.c LED part!
+UART_PUTCHAR //This is for redefining UART. Don't forget to delete the lcd_log.c LED part!
 {
 	HAL_UART_Transmit(&uart_handle, (uint8_t*) &ch, 1, 0xFFFF);
 	return ch;
@@ -107,32 +160,75 @@ UART_PUTCHAR //This is for redefining UART. Don't forget to decomment the lcd_lo
 
 void EXTI15_10_IRQHandler() //This states what we have to do to cause an interrupt.
 {
-	HAL_GPIO_EXTI_IRQHandler(button.Pin);
+	HAL_GPIO_EXTI_IRQHandler(button.Pin); //Which is the push of the button.
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) //This is called when the interrupt happens.
 {
-	printf("Button Pushed!\r\n");
+	if (game_state == BLINKING) {
+		printf("Button Pushed!\r\n");
 
-	uint32_t random_number;
-	HAL_RNG_GenerateRandomNumber(&rng_handle, &random_number);
-	random_iteration = random_number % 10;
+		uint32_t random_number;
+		HAL_RNG_GenerateRandomNumber(&rng_handle, &random_number);
+		random_iteration = 2 * (random_number % 10 + 1);
 
-	printf("Random number generated! It is %d\r\n", random_iteration);
-	HAL_TIM_Base_Start_IT(&tim_handle); //This starts up the timer
+		printf("Random number generated! It is %d\r\n", random_iteration / 2);
+		HAL_TIM_Base_Start_IT(&tim_handle); //This starts up the timer
+	}
+
+	if (game_state == INPUT) {
+		push_counter++;
+		printf("Now you pushed the button %d times!\r\n", push_counter);
+		TIM3->CNT = 0;
+	}
+
 }
 
 void TIM2_IRQHandler() {
 	HAL_TIM_IRQHandler(&tim_handle);
 }
 
+void TIM3_IRQHandler() {
+	HAL_TIM_IRQHandler(&tim_handle2);
+}
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) //By default, this is called when the timer's period is over
 {
-	if (random_counter != random_iteration) {
-		BSP_LED_Toggle(LED_GREEN);
-		random_counter++;
+	if (game_state == BLINKING) {
+		if (random_counter != random_iteration) {
+			BSP_LED_Toggle(LED_GREEN);
+			random_counter++;
+		} else {
+			HAL_TIM_Base_Stop_IT(&tim_handle); //This stops the timer
+			printf("So, how many blinks have you count?\r\n");
+			game_state = INPUT;
+			HAL_TIM_Base_Start_IT(&tim_handle2);
+		}
 	}
 
+	if (htim->Instance == TIM3) {
+		if (game_state == INPUT) {
+			printf("Gamestate is input\r\n");
+			if (!ignored_first_interrupt) {
+				ignored_first_interrupt = 1;
+				return;
+			}
+			HAL_TIM_Base_Stop_IT(&tim_handle2);
+			game_state = RESULT;
+		}
+
+		if (game_state == RESULT) {
+			if (push_counter == random_iteration / 2) {
+				__HAL_TIM_SET_COMPARE(&tim_handle3, TIM_CHANNEL_1, 100);
+				printf("Yes, it was %d blinks! You win!\r\n",
+						random_iteration / 2);
+			} else {
+				__HAL_TIM_SET_COMPARE(&tim_handle3, TIM_CHANNEL_1, 20);
+				printf("No, it was %d blinks! You lose!\r\n",
+						random_iteration / 2);
+			}
+		}
+	}
 }
 
 static void SystemClock_Config(void) {
